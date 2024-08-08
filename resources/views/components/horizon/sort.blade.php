7c3704de200c66187ecb8a0550b699543c9da970
Lucas Vigneron <lucas.vigneron@adeliom.com>
2024-08-08 14:44:17 +0200

    feat(components): improve basic sort field

--- resources/views/components/horizon/sort.blade.php
+++ resources/views/components/horizon/sort.blade.php
@@ -1,8 +1,11 @@
-<div>
-    Trier par
+@isset($options)
+    <div>
+        Trier par
 
-    <select @isset($model) wire:model="{{ $model }}@endisset">
-        <option value="date.desc">Plus récent au plus ancien</option>
-        <option value="date.asc">Plus ancien au plus récent</option>
-    </select>
-</div>
+        <select @isset($model) wire:model="{{ $model }}@endisset">
+            @foreach($options as $value => $label)
+                <option value="{{ $value }}">{{ $label }}</option>
+            @endforeach
+        </select>
+    </div>
+@endisset
