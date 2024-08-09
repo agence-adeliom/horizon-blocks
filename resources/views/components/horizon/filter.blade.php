c71f5e0538afd6e4435feec6358a15e1b2bc2f07
Lucas Vigneron <lucas.vigneron@adeliom.com>
2024-08-09 09:52:41 +0200

    feat(components): add select filter placeholder

--- resources/views/components/horizon/filter.blade.php
+++ resources/views/components/horizon/filter.blade.php
@@ -1,4 +1,6 @@
 @php
+    $placeholder = $value['placeholder'] ?? null;
+
     if(!isset($withEmpty)){
         $withEmpty = true;
     }
@@ -9,7 +11,9 @@
         @case('select')
             <select @if($model) wire:model="{{ $model }}" @endif>
                 @if($withEmpty)
-                    <option value=""></option>
+                    <option value="" selected>
+                        {{ $placeholder ?? 'Sélectionner' }}
+                    </option>
                 @endif
                 @foreach($value['choices'] as $choice)
                     <option value="{{ $choice['slug'] }}">
