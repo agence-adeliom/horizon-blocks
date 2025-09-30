d5227c19adabf2a3548fc911d3463e6299968942
emmadeliom <emma.louviot@adeliom.com>
2025-09-30 16:18:44 +0200

    fix(faqBlock): add buttons group in blade

--- resources/views/blocks/content/faq.blade.php
+++ resources/views/blocks/content/faq.blade.php
@@ -4,25 +4,30 @@
             @json($context['structuredData'])
         </script>
     @endif
-
+    
     <div class="grid-12">
         <div class="lg:col-span-5 lg:mr-12">
-            @isset($fields['img'])
+            @if (!empty($fields['img']))
                 <x-media.img :image="$fields['img']" class="w-20 mb-3" size="thumbnail" />
-            @endisset
-
-            @isset($fields['uptitle'])
+            @endif
+            
+            @if (!empty($fields['uptitle']))
                 <x-typography.uptitle :content="$fields['uptitle']" />
-            @endisset
-
-            @isset($fields['title'])
+            @endif
+            
+            @if (!empty($fields['title']))
                 <x-typography.heading :fields="$fields['title']" size="3" />
-            @endisset
-
-            @isset($fields['wysiwyg'])
+            @endif
+            
+            @if (!empty($fields['wysiwyg']))
                 <x-typography.text :content="$fields['wysiwyg']" class="pt-medium" />
-            @endisset
-
+            @endif
+            
+            @if (!empty($fields['buttons']))
+                <x-action.buttons :buttons="$fields['buttons']"
+                                  class="mt-button-text-mobile lg:mt-button-text-desktop" />
+            @endif
+        
         </div>
         <div class="lg:col-span-7 flex flex-col gap-medium">
             @if (@isset($fields['questions']) && $fields['questions'])
