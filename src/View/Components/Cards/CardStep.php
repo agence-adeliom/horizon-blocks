53e90a9b717fb8aaf94b0d9f3b39cdad54185d52
emmadeliom <emma.louviot@adeliom.com>
2026-07-29 17:47:35 +0200

    refactor(accordion): remove FaqBlock and rename admin images

--- src/View/Components/Cards/CardStep.php
+++ src/View/Components/Cards/CardStep.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Adeliom\HorizonBlocks\View\Components\Cards;
 
-use App\PostTypes\FAQ;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
