42529be35f6d3c7a299694e41cb7aa04f93f7fdc
emmadeliom <emma.louviot@adeliom.com>
2026-07-29 15:29:04 +0200

    fix(pagination): quote scrollToAnchor argument in Alpine expression
    L'interpolation $idToScroll rendait scrollToAnchor(listing-container),
    évalué par Alpine comme une soustraction de deux identifiants JS
    inexistants (ReferenceError au clic). L'argument doit être une string.

--- resources/views/components/horizon/pagination.blade.php
+++ resources/views/components/horizon/pagination.blade.php
@@ -84,7 +84,7 @@
                 @if ($current > 1)
                     <a @class([$baseButtonClass]) title="{{ $previousLabel }}"
                        href="{{ request()->fullUrlWithQuery(['pagination' => $current - 1]) }}"
-                       @click.prevent="scrollToAnchor({{$idToScroll}})"
+                       @click.prevent="scrollToAnchor('{{ $idToScroll }}')"
                        @if ($handle) wire:click.prevent="{{ $handle }}(
                        @if($extraHandleParamsFirst && $extraHandleParams) {{$extraHandleParams}} @endif{{ $current - 1 }}@if(!$extraHandleParamsFirst && $extraHandleParams) {{ $extraHandleParams }} @endif )" @endif>
                         <x-far-angle-left class="icon-20" />
@@ -103,7 +103,7 @@
                     @elseif($page != $separator)
                         <a @class([$baseNumberClass]) title="Page {{ $page }}"
                            href="{{ request()->fullUrlWithQuery(['pagination' => $page]) }}"
-                           @click.prevent="scrollToAnchor({{$idToScroll}})"
+                           @click.prevent="scrollToAnchor('{{ $idToScroll }}')"
                            @if ($handle) wire:click.prevent="{{ $handle }}(
                            @if($extraHandleParamsFirst && $extraHandleParams) {{ $extraHandleParams }} @endif {{$page}} @if(!$extraHandleParamsFirst && $extraHandleParams) {{$extraHandleParams}} @endif )" @endif>{{ $page }}</a>
                     @else
@@ -117,7 +117,7 @@
                 @if ($current < $pages)
                     <a @class([$baseButtonClass]) title="{{ $nextLabel }}"
                        href="{{ request()->fullUrlWithQuery(['pagination' => $current + 1]) }}"
-                       @click.prevent="scrollToAnchor({{$idToScroll}})"
+                       @click.prevent="scrollToAnchor('{{ $idToScroll }}')"
                        @if ($handle) wire:click.prevent="{{ $handle }}(
                        @if($extraHandleParamsFirst && $extraHandleParams) {{$extraHandleParams}} @endif {{ $current + 1 }} @if(!$extraHandleParamsFirst && $extraHandleParams) {{$extraHandleParams}} @endif )" @endif>
                         <x-far-angle-right class="icon-20" />
